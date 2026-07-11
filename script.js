cb1d7920fa64fe1aca246317e2bb566ece00a7f0
Zigarkhan999 <muhammadilyas13212@gmail.com>
2026-07-11 12:25:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 let currentQuestion = 0;
+let score = 0;
+
+let buttons = document.querySelectorAll("button");
+
+function loadQuestion() {
 
 document.getElementById("question").innerHTML =
 questions[currentQuestion].question;
 
-let buttons = document.querySelectorAll("button");
-
 for(let i=0; i<4; i++){
-    buttons[i].innerHTML = questions[currentQuestion].options[i];
+
+buttons[i].innerHTML =
+questions[currentQuestion].options[i];
+
+buttons[i].onclick = function(){
+
+if(i === questions[currentQuestion].answer){
+
+alert("✅ Correct!");
+score++;
+
+}else{
+
+alert("❌ Wrong!");
+
 }
+
+currentQuestion++;
+
+if(currentQuestion < questions.length){
+
+loadQuestion();
+
+}else{
+
+document.body.innerHTML =
+"<h1 style='text-align:center'>Quiz Finished 🎉<br>Score: "
++ score + " / " + questions.length + "</h1>";
+
+}
+
+};
+
+}
+
+}
+
+loadQuestion();
