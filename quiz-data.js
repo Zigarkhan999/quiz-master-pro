2fb1eed0f128f7303ff65c223725557666aafdef
Zigarkhan999 <muhammadilyas13212@gmail.com>
2026-07-11 12:19:29 +0500

    Add quiz questions

---
+++ quiz-data.js
@@ -0,0 +1,21 @@
+const questions = [
+
+{
+question: "What is the capital of Pakistan?",
+options: ["Lahore", "Islamabad", "Karachi", "Quetta"],
+answer: 1
+},
+
+{
+question: "2 + 2 = ?",
+options: ["3", "4", "5", "6"],
+answer: 1
+},
+
+{
+question: "What color is the sky?",
+options: ["Blue", "Green", "Red", "Black"],
+answer: 0
+}
+
+];
