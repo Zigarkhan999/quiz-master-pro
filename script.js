0ca2454fc82c75dcc1b1928bdd187a9675aaf239
Zigarkhan999 <muhammadilyas13212@gmail.com>
2026-07-11 12:22:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.querySelectorAll("button").forEach(function(button){
-    button.onclick = function(){
-        alert("You selected: " + this.innerHTML);
-    };
-});
+let currentQuestion = 0;
+
+document.getElementById("question").innerHTML =
+questions[currentQuestion].question;
+
+let buttons = document.querySelectorAll("button");
+
+for(let i=0; i<4; i++){
+    buttons[i].innerHTML = questions[currentQuestion].options[i];
+}
