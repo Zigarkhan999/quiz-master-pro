0f87d7829fa82133b64944cae5ed630b68ded0ae
Zigarkhan999 <muhammadilyas13212@gmail.com>
2026-07-12 15:14:12 +0500

    Create chapter1.js

---
+++ mdcat/biology/chapter1.js
@@ -0,0 +1,36 @@
+const chapter1 = [
+
+{
+question: "Water is described as a polar molecule because:",
+options: [
+"It contains carbon",
+"It has unequal sharing of electrons",
+"It is acidic",
+"It has no hydrogen"
+],
+answer: 1
+},
+
+{
+question: "Which element forms the backbone of organic molecules?",
+options: [
+"Oxygen",
+"Nitrogen",
+"Carbon",
+"Hydrogen"
+],
+answer: 2
+},
+
+{
+question: "Proteins are made up of:",
+options: [
+"Fatty acids",
+"Amino acids",
+"Monosaccharides",
+"Nucleotides"
+],
+answer: 1
+}
+
+];
